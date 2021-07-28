c4e52aad84f2f0646a589b386a87286da319dd09
Serhii Serhiichuk <serhii.serhiichuk@materialise.kiev.ua>
2021-07-28 11:44:16 +0300

    add additional class with params

--- PrimeNumber.UnitTests/PrimeNumberUnitTest.cpp
+++ PrimeNumber.UnitTests/PrimeNumberUnitTest.cpp
@@ -64,6 +64,19 @@ TEST_P(PrimeNumberCalculatorTestWithParams, Returns_6_Numbers_When_N_Equal_16_15
 INSTANTIATE_TEST_SUITE_P(Returns_6, PrimeNumberCalculatorTestWithParams, Values(16, 15, 14, 13));
 
 
+class PrimeNumberCalculatorTestWithParams2 :public PrimeNumberCalculatorTestNotTemplate, public WithParamInterface<size_t>
+{
+
+};
+
+TEST_P(PrimeNumberCalculatorTestWithParams2, Returns_0_Numbers_When_N_Equal_0_1)
+{
+	result = calc->GetPrimeNumbers<size_t>(GetParam());
+	EXPECT_THAT(result.size(), 0);
+}
+INSTANTIATE_TEST_SUITE_P(Returns_6, PrimeNumberCalculatorTestWithParams2, Values(0,1));
+
+
 TEST_F(PrimeNumberCalculatorTestNotTemplate, Returns_0_Numbers_When_N_Equal_0)
 {
 	result = calc->GetPrimeNumbers<size_t>(0);
